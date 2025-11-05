a05a6bd58d69e0e01812e057eb4b7f729f5ed0ac
RhysGabila <rhysfredric.gabila@smc.pshs.edu.ph>
2025-11-05 14:18:00 +0800

    Activity_2.cpp

--- Activity_2.cpp
+++ Activity_2.cpp
@@ -7,7 +7,7 @@ cout << "Enter a number from 1 to 10: ";
 cin >> a;
 
 for (int i = 1; i <= 10; i++) {
-    std::cout << a << " x " << i << " \n";
+    cout << a << " x " << i <<  " = " << a*i << " \n";
 }
 
 system("PAUSE");
